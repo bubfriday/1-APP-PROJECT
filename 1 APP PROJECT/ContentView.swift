633b3bf8fee13b6f8160169e1a3dcf706a67c98c
Frida <moragraciafrida@gmail.com>
2023-06-26 13:30:38 -0500

    Initial Commit

---
+++ 1 APP PROJECT/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  1 APP PROJECT
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Text("Here's a cute puppy")
+                .font(.title)
+                .foregroundColor(Color.blue)
+            Image("puppy")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(80)
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ 1 APP PROJECT/__APP_PROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  __APP_PROJECTApp.swift
+//  1 APP PROJECT
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct __APP_PROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
